47333fc842255e5765e4aef22dca9ecb53342782
kavya-1103 <73887364+kavya-1103@users.noreply.github.com>
2021-02-11 17:53:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var ball;
+var database,bref,pos
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    bref=database.ref('ball/position')
+
+    bref.on("value",readPos,showError);
+
+}
+
+function draw(){
+    background("white");
+    if(pos!==undefined)
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+  database.ref('ball/position').set({x:pos.x+x,y:pos.y+y})
+}
+function readPos(data){
+pos=data.val();
+ball.x=pos.x
+ball.y=pos.y
+
+}
+
+function showError(){
+    console.log();
+}
+
